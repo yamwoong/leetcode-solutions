b8bd037bc6bc95d4bedf09877504c60a94ffd16f
Dae Woong Kim <0206woong@naver.com>
2025-06-19 23:30:19 +0100

    20250619

---
+++ JavaScript/easy/0028-Find-the-Index-of-the-First-Occurrence-in-a-String.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  console.log("needle", haystack.indexOf(needle));
+  return haystack.indexOf(needle);
+};
---
+++ JavaScript/easy/0169-Majority-Element.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let count = 0;
+  let candidate = null;
+
+  for (let num of nums) {
+    if (count === 0) candidate = num;
+    count += num === candidate ? 1 : -1;
+  }
+  return candidate;
+};
